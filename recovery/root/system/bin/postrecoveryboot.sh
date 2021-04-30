dadcb9473dd6a057c0b2be2be1d731e6a887e0ac
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-30 11:32:22 +0530

    Update postrecoveryboot.sh

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -6,14 +6,12 @@
 sleep 1
 mount -w /product > /dev/null 2>&1
 mount -w /vendor > /dev/null 2>&1
-mount -w /system_ext > /dev/null 2>&1
 mount -w /system_root > /dev/null 2>&1
 
 
 sleep 1
 umount /product > /dev/null 2>&1
 umount /vendor > /dev/null 2>&1
-umount /system_ext > /dev/null 2>&1
 umount /system_root > /dev/null 2>&1
 
 sleep 1
